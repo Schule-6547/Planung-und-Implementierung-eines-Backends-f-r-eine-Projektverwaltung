122d9f50ea27f0b12280e42975d211b9ce7be174
Leon Kunkel <leon.kunkel@wamoco.de>
2025-11-05 10:25:56 +0100

    PUIEBFEP-12: tests

---
+++ src/test/java/de/szut/lf8_starter/project/ProjectServiceTest.java
@@ -0,0 +1,33 @@
+package de.szut.lf8_starter.project;
+
+import de.szut.lf8_starter.employee.EmployeeEntity;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+public class ProjectServiceTest {
+    @Test
+    public void unassignEmployeeToProjectTest() {
+        ProjectRepository projectRepository = mock(ProjectRepository.class);
+        ProjectService service = new ProjectService(projectRepository);
+
+        ProjectEntity project = mock(ProjectEntity.class);
+        EmployeeEntity employee = mock(EmployeeEntity.class);
+        List<EmployeeEntity> employees = new ArrayList<>();
+        employees.add(employee);
+        when(project.getEmployees()).thenReturn(employees);
+
+        service.unassignEmployeeToProject(project, employee);
+
+        assertEquals(0, employees.size());
+        verify(project, times(1)).getEmployees();
+        verify(projectRepository, times(1)).save(project);
+
+    }
+}
