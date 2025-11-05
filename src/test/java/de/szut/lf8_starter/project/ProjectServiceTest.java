779f8c4b2e6f0ba0b3b1fda024df30060e23af1e
Leon Kunkel <leon.kunkel@wamoco.de>
2025-11-05 10:59:08 +0100

    PUIEBFEP-10: wip

--- src/test/java/de/szut/lf8_starter/project/ProjectServiceTest.java
+++ src/test/java/de/szut/lf8_starter/project/ProjectServiceTest.java
@@ -1,9 +1,11 @@
 package de.szut.lf8_starter.project;
 
 import de.szut.lf8_starter.employee.EmployeeEntity;
+import de.szut.lf8_starter.project.exception.EmployeeAlreadyAssignedToProjectException;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +30,36 @@ public class ProjectServiceTest {
         assertEquals(0, employees.size());
         verify(project, times(1)).getEmployees();
         verify(projectRepository, times(1)).save(project);
-
     }
+
+    @Test
+    public void assignEmployeeToProjectTest() {
+        ProjectRepository projectRepository = mock(ProjectRepository.class);
+        ProjectService service = new ProjectService(projectRepository, mock(EmployeeApiClient.class));
+
+        ProjectEntity project = mock(ProjectEntity.class);
+        EmployeeEntity employee = mock(EmployeeEntity.class);
+        List<EmployeeEntity> employees = new ArrayList<>();
+        when(project.getEmployees()).thenReturn(employees);
+
+        service.assignEmployeeToProject(project, employee);
+
+        verify(project, times(1)).getStartDate();
+        verify(project, times(1)).getPlannedEndDate();
+        verify(project, times(1)).getEmployees();
+//        if (isAvailableInPeriod(project.getStartDate(), project.getPlannedEndDate(), employee)) {
+//            project.getEmployees().add(employee);
+//            repository.save(project);
+//        } else {
+//            throw new EmployeeAlreadyAssignedToProjectException("Employee " + employee.getId() + " is already assigned in this period.");
+//        }
+    }
+
+//    private boolean isAvailableInPeriod(LocalDateTime start, LocalDateTime end, EmployeeEntity employee) {
+//        return readAll().stream().noneMatch(project -> {
+//            if (project.getStartDate() == null || project.getPlannedEndDate() == null) return false;
+//            if (!project.getEmployees().contains(employee)) return false;
+//            return start.isAfter(project.getStartDate()) && end.isBefore(project.getPlannedEndDate());
+//        });
+//    }
 }
